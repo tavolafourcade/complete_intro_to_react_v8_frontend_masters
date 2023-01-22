4703b11ff71f62dc2d9a16b2a994308ac7fcc6fc
tavolafourcade <tavolafourcade@gmail.com>
2023-01-22 14:47:21 -0500

    [ UPDATE ] Use of useState hook to update the value of the input field

--- adopt-me/src/SearchParams.jsx
+++ adopt-me/src/SearchParams.jsx
@@ -1,12 +1,15 @@
+import { useState } from "react"
+
 const SearchParams = () => {
-  const location = "Seattle, WA"
+  
+  const [ location, setLocation ] = useState('')
 
   return (
     <div className="search-params">
       <form>
         <label htmlFor="location">
           Location
-          <input id="location" value={location} placeholder="Location" />
+          <input id="location" value={location} placeholder="Location" onChange={e => setLocation(e.target.value)}/>
         </label>
         <button>Submit</button>
       </form>
