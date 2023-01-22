372ec7902f0ece348811c5a8b14424d7e38820fa
tavolafourcade <tavolafourcade@gmail.com>
2023-01-21 19:20:22 -0500

    [ ADD ] Eslint and Prettier

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,10 +1,10 @@
 const Pet = (props) => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', {}, props.name),
-    React.createElement('h2', {}, props.animal),
-    React.createElement('h2', {}, props.breed),
-  ])
-}
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
+  ]);
+};
 
 const App = () => {
   return React.createElement(
@@ -12,23 +12,23 @@ const App = () => {
     {},
     React.createElement("h1", {}, "Adopt Me!"),
     React.createElement(Pet, {
-      animal: 'Dog',
-      name: 'Luna',
-      breed: 'Havanese'
+      animal: "Dog",
+      name: "Luna",
+      breed: "Havanese",
     }),
     React.createElement(Pet, {
-      animal: 'Bird',
-      name: 'Pepper',
-      breed: 'Cockatiel'
+      animal: "Bird",
+      name: "Pepper",
+      breed: "Cockatiel",
     }),
     React.createElement(Pet, {
-      animal: 'Cat',
-      name: 'Doink',
-      breed: 'Mixed'
+      animal: "Cat",
+      name: "Doink",
+      breed: "Havanese",
     })
-  )
-}
+  );
+};
 
-const container = document.getElementById('root')
-const root = ReactDOM.createRoot(container)
-root.render(React.createElement(App))
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
