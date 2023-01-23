abb67778945f697894098e92a63419d7359fc045
tavolafourcade <tavolafourcade@gmail.com>
2023-01-22 20:51:50 -0500

    [ DELETE ] Unnecessary code

--- adopt-me/src/Pet.jsx
+++ adopt-me/src/Pet.jsx
@@ -12,7 +12,7 @@ const Pet = ({name, animal, breed, images, location, id}) => {
       </div>
       <div className='info'>
         <h1>{name}</h1>
-        <h2>${animal} - ${breed} - ${location}</h2>
+        <h2>{animal} - {breed} - {location}</h2>
       </div>
     </a>
   );
