400babad1cdcb5f35d5b64afb82a25f045262f4e
tavolafourcade <tavolafourcade@gmail.com>
2023-01-23 10:09:28 -0500

    [ ADD ] Implementation of Link for a header in the App component

--- adopt-me/src/App.jsx
+++ adopt-me/src/App.jsx
@@ -1,11 +1,14 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Link, BrowserRouter, Routes, Route } from 'react-router-dom';
 import SearchParams from "./SearchParams";
 import Details from './Details';
 
 const App = () => {
   return (
     <BrowserRouter>
+      <header>
+        <Link to='/'>Adopt Me!</Link>
+      </header>
       <Routes>
         <Route path='/details/:id' element={<Details/>}/>
         <Route path="/" element={<SearchParams />} />
