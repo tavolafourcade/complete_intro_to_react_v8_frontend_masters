ade92c19bd270a9f290cf6034cfdf749025806dd
tavolafourcade <tavolafourcade@gmail.com>
2023-01-23 20:11:36 -0500

    [ ADD ] USe of React QQuery for fetching breed list

--- adopt-me/src/useBreedList.jsx
+++ adopt-me/src/useBreedList.jsx
@@ -1,32 +1,40 @@
-import {useState, useEffect} from 'react'
+// import {useState, useEffect} from 'react'
+import { useQuery } from '@tanstack/react-query'
+import fetchBreedList from './fetchBreedList'
 
-const localCache = {}
+// const localCache = {}
 
 export default function useBreedList(animal) {
-const [ breedList, setBreedList ] = useState([])
-const [ status, setStatus ] = useState('unloaded')
 
-useEffect(() => {
-  if (!animal) {
-    setBreedList([])
-  } else if (localCache[animal]) {
-    setBreedList(localCache[animal])
-  } else {
-    requestBreedList()
-  }
+  // This is a simpler way to implement instead of using useEffect lines below
+  const results = useQuery(['breeds', animal], fetchBreedList)
 
-  async function requestBreedList() {
-    setBreedList([])
-    setStatus('loading')
+  return [results?.data?.breeds ?? [], results.status]
 
-    const res = await fetch(`http://pets-v2.dev-apis.com/breeds?animal=${animal}`)
-    const json = await res.json()
+// const [ breedList, setBreedList ] = useState([])
+// const [ status, setStatus ] = useState('unloaded')
 
-    localCache[animal] = json.breeds || []
-    setBreedList(localCache[animal])
-    setStatus('loaded')
-  }
-},[animal])
+// useEffect(() => {
+//   if (!animal) {
+//     setBreedList([])
+//   } else if (localCache[animal]) {
+//     setBreedList(localCache[animal])
+//   } else {
+//     requestBreedList()
+//   }
 
-return [breedList, status]
+//   async function requestBreedList() {
+//     setBreedList([])
+//     setStatus('loading')
+
+//     const res = await fetch(`http://pets-v2.dev-apis.com/breeds?animal=${animal}`)
+//     const json = await res.json()
+
+//     localCache[animal] = json.breeds || []
+//     setBreedList(localCache[animal])
+//     setStatus('loaded')
+//   }
+// },[animal])
+
+// return [breedList, status]
 }
