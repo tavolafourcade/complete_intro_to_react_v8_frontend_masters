e3fefba275a9f45e5f100e0750ebf5b7067f963c
tavolafourcade <tavolafourcade@gmail.com>
2023-01-23 20:33:20 -0500

    [ ADD ] Implementation of useQuery for caching data instead of useEffect

--- adopt-me/src/SearchParams.jsx
+++ adopt-me/src/SearchParams.jsx
@@ -1,45 +1,56 @@
-import { useState, useEffect } from "react"
+import { useState } from "react"
+import { useQuery } from "@tanstack/react-query"
 import Results from './Results'
 import useBreedList from "./useBreedList"
+import fetchSearch from "./fetchSearch"
 
 const ANIMALS = ['bird', 'cat', 'dog', 'rabbit', 'reptile']
 
 const SearchParams = () => {
-  
-  const [ location, setLocation ] = useState('')
+  const [ requestParams, setRequestParams ] = useState({
+    location: '',
+    animal: '',
+    breed: ''
+  })
+  // const [ location, setLocation ] = useState('')
   const [ animal, setAnimal ] = useState('')
-  const [ breed, setBreed ] = useState('')
-  const [ pets, setPets ] = useState([])
+  // const [ breed, setBreed ] = useState('')
+  // const [ pets, setPets ] = useState([])
   const [ breeds ] = useBreedList(animal)
 
-  useEffect(() => {
-    requestPets()
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+  const results = useQuery(['search', requestParams], fetchSearch)
+  const pets = results?.data?.pets ?? []
 
-  const requestPets = async() => {
-    const res = await fetch(`http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`)
-    const json = await res.json()
-    setPets(json.pets)
-  }
+  // useEffect(() => {
+  //   requestPets()
+  // // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [])
+
+  // const requestPets = async() => {
+  //   const res = await fetch(`http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`)
+  //   const json = await res.json()
+  //   // setPets(json.pets)
+  // }
 
   return (
     <div className="search-params">
       <form onSubmit={e => {
         e.preventDefault()
-        requestPets()
+
+        const formData = new FormData(e.target) // Browser API it will pull out all the data from the form into an object.
+        const obj = {
+          animal: formData.get('animal') ?? '',
+          breed: formData.get('breed') ?? '',
+          location: formData.get('location') ?? ''
+        }
+        setRequestParams(obj)
       }}>
         <label htmlFor="location">
           Location
           <input 
+          name='location'
           id="location" 
-          value={location} 
-          placeholder="Location" 
-          onChange={
-            e => {
-              setLocation(e.target.value)
-              setBreed('')
-            }}/>
+          placeholder="Location" />
         </label>
         <label htmlFor="animal">
           Animal
@@ -53,7 +64,7 @@ const SearchParams = () => {
 
         <label htmlFor="breed">
           Breed
-          <select id="breed" value={breed} onChange={e => setBreed(e.target.value)} disabled={breeds.length === 0}>
+          <select id="breed" name='breed' disabled={breeds.length === 0}>
             <option />
             {
             breeds.map(breed => <option key={breed}>{breed}</option>)
