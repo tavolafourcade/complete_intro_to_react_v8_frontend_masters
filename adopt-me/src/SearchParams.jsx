81a2d07f95d71d8a22e55b785e604b2d70cc5df0
tavolafourcade <tavolafourcade@gmail.com>
2023-03-10 19:38:14 -0500

    [ ADD ] Use of Context

--- adopt-me/src/SearchParams.jsx
+++ adopt-me/src/SearchParams.jsx
@@ -1,5 +1,6 @@
-import { useState } from "react"
+import { useState, useContext } from "react"
 import { useQuery } from "@tanstack/react-query"
+import AdoptedPetContext from "./AdoptedPetContext"
 import Results from './Results'
 import useBreedList from "./useBreedList"
 import fetchSearch from "./fetchSearch"
@@ -17,6 +18,7 @@ const SearchParams = () => {
   // const [ breed, setBreed ] = useState('')
   // const [ pets, setPets ] = useState([])
   const [ breeds ] = useBreedList(animal)
+  const [adoptedPet] = useContext(AdoptedPetContext)
 
   const results = useQuery(['search', requestParams], fetchSearch)
   const pets = results?.data?.pets ?? []
@@ -45,6 +47,13 @@ const SearchParams = () => {
         }
         setRequestParams(obj)
       }}>
+        {
+          adoptedPet ? (
+            <div className="pet image-container">
+              <img src={adoptedPet.images[0]} alt={adoptedPet.name}/>
+            </div>
+          ) : null
+        }
         <label htmlFor="location">
           Location
           <input 
